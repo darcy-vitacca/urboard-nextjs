c84c733899a3ae1c5c4ac30279c60f7e2cec0f45
Darcy Vitacca <darcy.vitacca@seventhbeam.com>
2022-09-30 22:04:39 +0800

    sidebar wip

--- src/constants/nav.ts
+++ src/constants/nav.ts
@@ -1,4 +1,4 @@
-import { HomeIcon, FolderIcon, PencilSquareIcon, TrashIcon } from '@heroicons/react/24/outline'
+import { HomeIcon, FolderIcon, PencilSquareIcon, TrashIcon, Cog8ToothIcon } from '@heroicons/react/24/outline'
 
 export type INavItem = {
     toolTip: string;
@@ -6,11 +6,17 @@ export type INavItem = {
     href: string;
 };
 
+
 export const navItems: INavItem[] = [
     {
         toolTip: "Home",
         Icon: HomeIcon,
         href: "/"
+    },
+    {
+        toolTip: "Settings",
+        Icon: Cog8ToothIcon,
+        href: "/settings"
     },
     {
         toolTip: "Add Folder",
@@ -28,3 +34,9 @@ export const navItems: INavItem[] = [
         href: "/"
     },
 ];
+
+export const logoutMenuItem: INavItem = {
+    toolTip: "Logout",
+    Icon: HomeIcon,
+    href: "/"
+}
