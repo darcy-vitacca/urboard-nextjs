a8c3e327d4969b2297c86cdd666c1671fb8b3b7b
Darcy Vitacca <vitacca6@hotmail.com>
2022-12-09 20:03:42 +1100

    fixed config

--- src/utils/gtag.ts
+++ src/utils/gtag.ts
@@ -2,6 +2,8 @@ export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_MEASUREME
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: URL): void => {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  //@ts-ignore
   window.gtag("config", GA_TRACKING_ID, {
     page_path: url,
   });
